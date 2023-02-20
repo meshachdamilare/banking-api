c4b466b9f7ac0c4582ef8e05370e6b2fbd8bce30
Meshach Odejide <meshacholuwadamilare@gmail.com>
2023-02-20 08:51:14 +0100

    update

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"github.com/meshachdamilare/banking-api/database"
+	"log"
+)
 
+func main() {
+	//err := database.MigratePostgres()
+	//if err != nil {
+	//	panic(err)
+	//}
+	err := database.MigrateImmuDB()
+	if err != nil {
+		log.Println(err)
+	}
 }
